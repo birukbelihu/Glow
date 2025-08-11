49561b57eaf611403fef9b49bade3ef608c56d78
Biruk Belihu <birukbelihu1995@gmail.com>
2025-08-11 09:44:19 +0300

    v1.3.1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -23,7 +23,7 @@ public class Main {
         Glow.println("Glow Java", "bold underline magenta");
         Glow.println("[bg-black cyan]This is a test of the Glow library.[/]");
 
-        Glow.println("[rgb(255,56,0)]Red text[/]");
-        Glow.println("[hex(#00FF00)]Green text[/]");
+        Glow.println("[rgb(255,56,0) italic underline]Red text[/]");
+        Glow.println("[hex(#00FF00) bold dim bg-blue]Green text[/]");
     }
 }

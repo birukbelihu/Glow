a098ca6328f26f652e5a236582658782d6ff6603
Biruk Belihu <birukbelihu1995@gmail.com>
2025-08-11 09:46:17 +0300

    v1.3.1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ public class Main {
                 [cyan bold]Glow[/] is a lightweight, [bold]markdown-style console text formatter[/] for Java. It enables developers to output styled text in the terminal using simple and readable tags like `[red bold]Error[/]`.
                 """);
 
-        Glow.println("[green]Success: [bold]42 tests passed![/]");
+        Glow.println("[green]Success: [bold]42 tests passed!!![/]");
 
         Glow.println("[bg-yellow black italic]Warning:[/] Low disk space.");
 
